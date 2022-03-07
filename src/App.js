e8bc63c1682484334ff0041b41c793ee866f82e9
Arpit <arpitmh23@gmail.com>
2022-03-07 22:34:48 +0530

    To display hello with user name

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import React, { Component } from 'react';
 import bridge from './bridge.jpg'
 
 class App extends React.Component {
-  url = "https://www.bridgelabz.com/"
+  url = "https://www.bridgelabz.com"
   constructor() {
     super();
     this.state = {
-      title: 'Hello from bridgelabz'
+      userName: ""
     }
   }
 
   //onclick function
-  onClick = ($event) => {
-    console.log("save button is clicked", $event);
+  onClick = (event) => {
+    console.log("save button is clicked", event);
     window.open(this.url, "_blank");
+  }
+  
+  //onNameChangefunction
+  onNameChange = (event) => {
+    console.log("click", event.target.value);
+    this.setState({ userName: event.target.value })
   }
   render() {
     return (
-      <div >
-        <h1> {this.state.title} </h1>
-        <img src={bridge} onClick={this.onClick}
-          alt="The bridgelabz logo : bridge to the employment " />
-      </div >
+      <>
+        <div >
+          <h1>Hello {this.state.userName} from bridgelabz </h1>
+          <img src={bridge} onClick={this.onClick}
+            alt="The bridgelabz logo : bridge to the employment " />
+        </div >
+        <div>
+          <input onChange={this.onNameChange} />
+        </div>
+      </>
     );
   }
 }
